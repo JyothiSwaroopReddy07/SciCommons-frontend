4c7dee5439dbb1cfbd2e8a22a99eccf19aac6889
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-09-04 22:25:00 +0530

    added versioning of reviews, comments

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Comments.css";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import ToastMaker from "toastmaker";
 import "toastmaker/dist/toastmaker.css";
 import axios from "axios";
+import {IoIosArrowBack,IoIosArrowForward} from "react-icons/io";
+
 
 const ArticleCommentModal = ({setShowCommentModal, article, Comment, handleComment }) => {
 
@@ -15,8 +17,6 @@ const ArticleCommentModal = ({setShowCommentModal, article, Comment, handleComme
   const handleCommentChange = (event) => {
       setComment(event);
   }
-
-
 
   const handleSubmit = async(e) => {
       e.preventDefault();
@@ -46,15 +46,13 @@ const ArticleCommentModal = ({setShowCommentModal, article, Comment, handleComme
             });
       } catch(err){
           setLoading(false);
-          if(err.res.data.error){
-              ToastMaker(err.res.data.error, 3000, {
+              ToastMaker("Comment posting failed!!!", 3000, {
                   valign: "top",
                   styles: {
                     backgroundColor: "red",
                     fontSize: "20px",
                   },
               });
-          }
           console.log(err);
       }
   }
@@ -109,6 +107,105 @@ const ArticleCommentModal = ({setShowCommentModal, article, Comment, handleComme
   );
 };
 
+const ArticleCommentEditModal = ({setShowEditModal, article, Comment, version, handleVersion }) => {
+
+  const [title, setTitle] = useState(version.Title);
+  const [comment, setComment] = useState(version.Comment);
+  const [loading, setLoading] = useState(false);
+
+  const handleCommentChange = (event) => {
+      setComment(event);
+  }
+
+  const handleSubmit = async(e) => {
+      e.preventDefault();
+      setLoading(true);
+      const config={
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+          }
+      };
+      const comment_Type = (article.isArticleModerator || article.isArticleReviewer || article.isAuthor)?"officialcomment":"publiccomment";
+      try {
+          const res = await axios.post(`https://scicommons-backend.onrender.com/api/comment/`,
+          {Title: title,Comment: comment, article: article.id, Type: 'comment', comment_Type:comment_Type, tag: "public",parent_comment:null, version:Comment.id}, 
+          config);
+          console.log(res);
+          setLoading(false);
+          setTitle("");
+          setComment("");
+          await handleVersion(res.data.comment);
+          ToastMaker("Comment Posted Successfully!!!", 3000, {
+              valign: "top",
+              styles: {
+                backgroundColor: "green",
+                fontSize: "20px",
+              },
+            });
+      } catch(err){
+          setLoading(false);
+          ToastMaker("Comment posting failed!!!", 3000, {
+            valign: "top",
+            styles: {
+              backgroundColor: "red",
+              fontSize: "20px",
+            },
+          });
+          console.log(err);
+      }
+  }
+
+
+  return (
+      <>
+          <div className="fixed inset-0 flex items-center justify-center w-full z-50 bg-gray-800 bg-opacity-50">
+              <div className="flex items-center justify-center w-5/6 my-2 p-4">
+                  <div className="bg-slate-200 p-6 rounded-lg max-h-5/6 overflow-hidden w-full">
+                  <h2 className="text-xl font-semibold mb-4">Post a Comment</h2>
+                  <form onSubmit={handleSubmit}>
+                      <div className="mb-4">
+                          <label htmlFor="title" className="block font-medium mb-1">
+                              Title
+                          </label>
+                          <input
+                              type="text"
+                              id="Title"
+                              value={title}
+                              name="Title"
+                              onChange={(e) => setTitle(e.target.value)}
+                              className="w-full border border-gray-300 rounded p-2"
+                              required
+                          />
+                      </div>
+                      <div className="mb-4">
+                          <label htmlFor="comment" className="block font-medium mb-1">
+                              Comment
+                          </label>
+                          <ReactQuill theme="snow" className="bg-white w-full p-2 mb-4 resize-none border rounded max-h-[40vh] overflow-y-auto" value={comment} onChange={handleCommentChange}/>
+                      </div>
+                      <button
+                      type="submit"
+                      className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 text-white rounded mr-2 font-semibold"
+                      >
+                          {
+                              loading ? "Posting..." : "Post Comment"
+                          }
+                      </button>
+                      <button
+                          className="px-4 py-2 bg-red-500 text-white rounded font-semibold"
+                          onClick={()=>{setShowEditModal(false)}}
+                      >
+                          Close
+                      </button>
+                  </form>
+              </div>
+          </div>
+      </div>
+      </>
+  );
+};
+
 const Comments = ({ comment, article, colour }) => {
 
   const [loading, setLoading] = useState(false);
@@ -117,6 +214,9 @@ const Comments = ({ comment, article, colour }) => {
   const [rating, setRating] = useState(comment.userrating?comment.userrating:0);
   const [overallrating, setOverallRating] = useState(comment.commentrating?comment.commentrating:0);
   const [showCommentModal, setShowCommentModal] = useState(false);
+  const [versions,setVersions] = useState([comment,...comment.versions]);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [index,setIndex] = useState(comment.versions.length);
 
   const colorClasses = {
     0: 'bg-slate-100',
@@ -152,8 +252,18 @@ const Comments = ({ comment, article, colour }) => {
 
   const handleComment = async(res) => {
     const newReply = [...repliesData,res];
+    let newVersions = [...versions];
+    newVersions[index].replies+=1;
     setRepliesData(newReply);
+    setVersions(newVersions);
     setShowCommentModal(false)
+  }
+
+  const handleVersion = async(res) => {
+    const newVersion = [...versions,res];
+    setVersions(newVersion);
+    setIndex(newVersion.length-1);
+    setShowEditModal(false);
   }
 
   const styleLinksWithColor = (htmlContent) => {
@@ -202,6 +312,9 @@ const Comments = ({ comment, article, colour }) => {
     await handleLike(event);
   };
 
+  const handleRefresh = async() => {
+    setRepliesData([]);
+  }
   const handleLike = async(event) => {
     const config={
       headers: {
@@ -222,13 +335,6 @@ const Comments = ({ comment, article, colour }) => {
       });
     } catch(err){
       console.log(err);
-      ToastMaker(err.response.data.error, 3000, {
-        valign: "top",
-        styles: {
-          backgroundColor: "red",
-          fontSize: "20px",
-        },
-    });
     }
   }
 
@@ -237,8 +343,7 @@ const Comments = ({ comment, article, colour }) => {
     setRepliesData(newReply);
   };
 
-  const handleReply = async (e) => {
-    e.preventDefault();
+  const handleReply = async () => {
     setLoading(true);
     const config = {
       headers: {
@@ -246,7 +351,7 @@ const Comments = ({ comment, article, colour }) => {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
       params: {
-        parent_comment: comment.id,
+        parent_comment: versions[index].id,
         article: article.id,
       },
     };
@@ -265,8 +370,8 @@ const Comments = ({ comment, article, colour }) => {
   const fillLoad = () => {
     if (repliesData.length === 0) {
       return `Load replies`;
-    } else if (comment.replies > repliesData.length) {
-      return `Load ${comment.replies - repliesData.length} more replies`;
+    } else if (versions[index].replies > repliesData.length) {
+      return `Load ${versions[index].replies - repliesData.length} more replies`;
     } else {
       return "";
     }
@@ -276,16 +381,17 @@ const Comments = ({ comment, article, colour }) => {
   return (
     <>
       <div className={`mb-2 w-full ${colorClasses[colour]} shadow-lg min-w-[200px] rounded px-4 py-2 overflow-x-auto`} data-commentid={comment.id}>
-          <div className="flex flex-row items-center" style={{cursor:"pointer"}} onClick={()=>{setShow(!show)}}>
+          <div className="flex flex-row items-center justify-between" >
+            <div className="flex flex-row items-center"  style={{cursor:"pointer"}} onClick={()=>{setShow(!show)}}>
               <div className="flex flex-row items-center">
                 <span className='font-bold  relative text-xl text-gray-600 leading-[1.25rem]'>
-                  {comment.Title}
+                  {versions[index].Title}
                 </span>
                 <span className=" text-[#777] font-[400] text-[0.55 rem] ml-2  p-2">
-                    • by {comment.personal?"you":comment.user}
+                    • by {versions[index].personal?"you":versions[index].user}
                 </span>
                 <span className="text-xs text-slate-400">
-                  • {findTime(comment.Comment_date)} •
+                  • {findTime(versions[index].Comment_date)} •
                 </span>
               </div>
               {comment.Type==="review" && 
@@ -353,6 +459,12 @@ const Comments = ({ comment, article, colour }) => {
                         </svg>
                     </div>
                   }
+                </div>
+                <div className="flex">
+                    <button onClick={async(e)=>{e.preventDefault();await handleRefresh();setIndex(index-1)}} style={{cursor:"pointer"}} disabled={index===0} className={index===0?"text-gray-300":""}><IoIosArrowBack className="w-6 h-6"/></button>
+                    {index+1} / {versions.length}
+                    <button onClick={async(e)=>{e.preventDefault();await handleRefresh();setIndex(index+1)}} style={{cursor:"pointer"}} disabled={index===versions.length-1} className={index===versions.length-1?"text-gray-300":""}><IoIosArrowForward className="w-6 h-6"/></button>
+                </div>
           </div>
           {show && (
           <>
@@ -367,7 +479,7 @@ const Comments = ({ comment, article, colour }) => {
                 Comment:
               </div>
               <ReactQuill
-                value={styleLinksWithColor(comment.Comment)}
+                value={styleLinksWithColor(versions[index].Comment)}
                 readOnly={true}
                 modules={{toolbar: false}}
               />
@@ -380,7 +492,7 @@ const Comments = ({ comment, article, colour }) => {
                     <div className="text-sm mr-2 font-semibold text-green-600">
                       Rating of Comment:
                     </div>
-                    {comment.personal === false &&
+                    {versions[index].personal === false &&
                     <div className="w-16 my-1 mr-2 relative">
                       <input
                         type="range"
@@ -389,7 +501,7 @@ const Comments = ({ comment, article, colour }) => {
                         step="1"
                         value={rating}
                         onChange={handleSliderChange}
-                        className="w-full h-2 appearance-none bg-transparent outline-none"
+                        className="w-full h-1 appearance-none bg-transparent outline-none"
                         style={{
                           backgroundImage: `linear-gradient(to right, #38A169, #38A169 ${rating * 20}%, #E2E8F0 ${rating * 20}%, #E2E8F0 100%)`,
                           borderRadius: '10px',
@@ -413,20 +525,20 @@ const Comments = ({ comment, article, colour }) => {
                       {overallrating}
                     </div>
                   </div>
-                <div className="flex flex-row">
-                <span className="box-content text-white bg-[#4d8093] text-[0.55 rem] border-solid ml-2 mr-2 md:font-bold p-2 pt-0 rounded" style={{cursor:"pointer"}} onClick={()=>{setShowCommentModal(true);}}>
+                <div className="mt-2 flex flex-row">
+                  {comment.personal && <span className="box-content text-white bg-[#4d8093] text-md border-solid ml-2 mr-2 md:font-bold p-2 pt-0 rounded" style={{cursor:"pointer"}} onClick={()=>{setShowEditModal(true);}}>
                     edit comment
-                  </span>
-                  <span className="box-content text-white bg-[#4d8093] text-[0.55 rem] border-solid ml-2 md:font-bold p-2 pt-0 rounded" style={{cursor:"pointer"}} onClick={()=>{setShowCommentModal(true);}}>
+                  </span>}
+                  <span className="box-content text-white bg-[#4d8093] text-md border-solid ml-2 md:font-bold p-2 pt-0 rounded" style={{cursor:"pointer"}} onClick={()=>{setShowCommentModal(true);}}>
                     reply
                   </span>
                 </div>
               </div>
-            <div className="ml-10">
+            <div className="mt-3 ml-10">
             {repliesData.length > 0 &&
               repliesData.map((reply) => <Comments key={reply.id} comment={reply} article={article} colour={colour===1?0:1}/>)}
             </div>
-            {comment.replies > 0 && (
+            {versions[index].replies > 0 && (
               <button style={{cursor:"pointer"}} onClick={handleReply} className="ml-5 text-xs mt-4">
                 {loading ? (
                   <span className="text-gray-600 font-bold">Loading...</span>
@@ -438,7 +550,8 @@ const Comments = ({ comment, article, colour }) => {
 
           </>)
           }
-          {showCommentModal && <ArticleCommentModal setShowCommentModal={setShowCommentModal} article={article} Comment={comment} handleComment={handleComment}/>}
+          {showCommentModal && <ArticleCommentModal setShowCommentModal={setShowCommentModal} article={article} Comment={versions[index]} handleComment={handleComment}/>}
+          {showEditModal && <ArticleCommentEditModal setShowEditModal={setShowEditModal} article={article} Comment={comment} version={versions[versions.length-1]} handleVersion={handleVersion}/>}
       </div>
     </>
   );
