e9eccd1535d6a09e5ea3e88248c5244ab35cbbdf
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-09-07 08:47:08 +0530

    modified login.jsx

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -58,7 +58,7 @@ const Login = () => {
 
         try {
             const response = await axios.post(
-              'http://127.0.0.1:8000/api/user/login/',
+              'https://scicommons-backend.onrender.com/api/user/login/',
               data,
               {
                 headers: {
