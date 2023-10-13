aa74980741975208f71b548e18d5035972dcdbee
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-10-13 06:16:31 +0530

    modified serializer

--- src/Components/ArticleCard/ArticleCard.jsx
+++ src/Components/ArticleCard/ArticleCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AiFillEye } from "react-icons/ai";
 import dayjs from "dayjs";
 import "./ArticleCard.css";
+import {AiFillHeart} from "react-icons/ai";
 
 const ArticleCard = ({ articles }) => {
   const formatCount = (count) => {
@@ -50,20 +51,7 @@ const ArticleCard = ({ articles }) => {
                         </span>
                       </span>
                       <span className="flex items-center text-gray-500">
-                        <svg
-                          className="text-rose-500 w-4 h-4 mr-2 fill-current"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                          />
-                        </svg>
+                        <AiFillHeart className="w-4 h-4 mr-2" />
                         <span className="text-lg font-bold">
                           {item.favourites == null
                             ? 0
